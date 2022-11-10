31a6e26072078310ce77900ba40b091fbd0f9fb2
yjshin1215 <112421766+yjshin1215@users.noreply.github.com>
2022-11-10 13:53:57 -0800

    finished hw8

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,7 @@
+let songsSecondHalf = document.getElementById("songs-second-half");
+
+function toggleSongs() {
+    songsSecondHalf.classList.toggle("hidden");
+}
+
+document.getElementById("toggle-button").onclick = toggleSongs;
